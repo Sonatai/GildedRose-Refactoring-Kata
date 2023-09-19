2f629f74cfb2d0a8240eef89c2246d0cae5c9863
Jenny Gebauer <se23m038@technikum-wien.at>
2023-09-19 20:15:59 +0200

    seperate individual item per if statement

--- csharpcore/GildedRose/GildedRose.cs
+++ csharpcore/GildedRose/GildedRose.cs
@@ -19,50 +19,54 @@ public class GildedRose
             if (item.Name == "Sulfuras, Hand of Ragnaros")
             {
                 //Nothing happen here :)
-            } else
+            } else if (item.Name == "Aged Brie")
             {
-                if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                if (item.Quality < 50)
                 {
-                    if (item.Quality > 0)
-                        item.Quality -= 1;
-                } else
-                {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality += 1;
-
-                        if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (item.SellIn < 11)
-                                if (item.Quality < 50)
-                                    item.Quality += 1;
-
-                            if (item.SellIn < 6)
-                                if (item.Quality < 50)
-                                    item.Quality += 1;
-                        }
-                    }
+                    item.Quality += 1;
                 }
 
                 item.SellIn -= 1;
 
                 if (item.SellIn < 0)
                 {
-                    if (item.Name != "Aged Brie")
-                    {
-                        if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (item.Quality > 0)
-                                item.Quality -= 1;
-                        } else
-                        {
-                            item.Quality -= item.Quality;
-                        }
-                    } else
-                    {
+                    if (item.Quality < 50)
+                        item.Quality += 1;
+                }
+            } else if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+            {
+                if (item.Quality < 50)
+                {
+                    item.Quality += 1;
+
+
+                    if (item.SellIn < 11)
                         if (item.Quality < 50)
                             item.Quality += 1;
-                    }
+
+                    if (item.SellIn < 6)
+                        if (item.Quality < 50)
+                            item.Quality += 1;
+                }
+
+                item.SellIn -= 1;
+
+                if (item.SellIn < 0)
+                {
+                    item.Quality = 0;
+                }
+            } else
+            {
+                if (item.Quality > 0)
+                    item.Quality -= 1;
+
+
+                item.SellIn -= 1;
+
+                if (item.SellIn < 0)
+                {
+                    if (item.Quality > 0)
+                        item.Quality -= 1;
                 }
             }
         }
