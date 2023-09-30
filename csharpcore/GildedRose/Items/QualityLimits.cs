6c294ab11b830e0e3a8ed52f7fbb2b093d1a9562
Sonatai <gebauerjenny@yahoo.de>
2023-09-30 16:20:40 +0200

    delete unused function

--- csharpcore/GildedRose/Items/QualityLimits.cs
+++ csharpcore/GildedRose/Items/QualityLimits.cs
@@ -4,10 +4,5 @@
     {
         public static readonly int QUALITY_UPPER_LIMIT = 49;
         public static readonly int QUALITY_LOWER_LIMIT = 0;
-
-        public static bool CheckIfQualityIsInLimit(int quality)
-        {
-            return quality >= QUALITY_LOWER_LIMIT && quality <= QUALITY_UPPER_LIMIT;
-        }
     }
 }
